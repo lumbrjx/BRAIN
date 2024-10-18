7e5e5a70398bd1677882ccd6ef627bbc46662c2c
lumbrjx <kaibitayeb01@gmail.com>
2024-10-18 21:18:47 +0100

    BugFix :bug: fix err body in zod validation

--- server/src/plugins/errHandler.ts
+++ server/src/plugins/errHandler.ts
@@ -2,16 +2,16 @@ import type { FastifyInstance } from "fastify";
 import { fastifyPlugin } from "fastify-plugin";
 
 const errorHandlerPlugin = async (fastify: FastifyInstance) => {
-  // Handle and parse ZodErrors
-  fastify.setErrorHandler((error, _request, reply) => {
-    if (error instanceof Error && error.constructor.name === "ZodError") {
-      reply.status(400).send({
-        success: false,
-        message: error.message,
-      });
-      return;
-    }
-  });
+	// Handle and parse ZodErrors
+	fastify.setErrorHandler((error, _request, reply) => {
+		if (error instanceof Error && error.constructor.name === "ZodError") {
+			reply.status(400).send({
+				ok: false,
+				message: error.message,
+			});
+			return;
+		}
+	});
 };
 
 
